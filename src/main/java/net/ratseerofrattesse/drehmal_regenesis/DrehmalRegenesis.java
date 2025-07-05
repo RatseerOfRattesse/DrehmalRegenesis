5b7ce04f10054770d546bb256ab1e06dd38650f5
TheHolyRat <ethant8251@gmail.com>
2025-07-04 22:13:42 -0700

    Initial commit

---
+++ src/main/java/net/ratseerofrattesse/drehmal_regenesis/DrehmalRegenesis.java
@@ -0,0 +1,17 @@
+package net.ratseerofrattesse.drehmal_regenesis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DrehmalRegenesis implements ModInitializer {
+	public static final String MOD_ID = "drehmal_regenesis";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("big snake");
+	}
+}
---
+++ src/main/java/net/ratseerofrattesse/drehmal_regenesis/DrehmalRegenesisClient.java
@@ -0,0 +1,10 @@
+package net.ratseerofrattesse.drehmal_regenesis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrehmalRegenesisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
